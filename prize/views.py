3efb1a1a0650ed96f907d020b2a557226a26cfac
Tashanah Muhando <tashanah.muhando@gmail.com>
2019-05-27 13:41:18 +0300

    projectlist post method added

--- prize/views.py
+++ prize/views.py
@@ -91,3 +91,10 @@ class ProfileList(APIView):
         serializers = ProfileSerializer(all_profile, many=True)
         return Response(serializers.data)
 
+    def post(self, request, format=None):
+        serializers =ProfileSerializer(data=request.data)
+        if serializers.is_valid():
+            serializers.save()
+            return Response(serializers.data, status=status.HTTP_201_CREATED)
+        return Response(serializers.errors, status=status.HTTP_400_BAD_REQUEST)    
+
