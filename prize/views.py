7f3d5ceef963dd206b67d6b0cb2c3ddc548c7864
Tashanah Muhando <tashanah.muhando@gmail.com>
2019-05-27 09:22:51 +0300

    post project function added

--- prize/views.py
+++ prize/views.py
@@ -14,3 +14,18 @@ def home(request):
     }
 
     return render(request,'project.html',context)
+
+@login_required(login_url='/accounts/login/')
+def post_project(request):
+    current_user = request.user
+    if request.method == 'POST':
+        upload_form = UserUploadProject(request.POST, request.FILES)
+        if upload_form.is_valid():
+            home = upload_form.save(commit=False)
+            home.profile =current_user
+            upload_form.save()
+        return redirect('home')
+    else:
+        upload_form = UserUploadProject()
+            
+    return render(request,'uploads.html',{"upload_form":upload_form,})
