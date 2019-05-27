cf85cd2d48886502d96af1626bd2786c5e7b4f4c
Tashanah Muhando <tashanah.muhando@gmail.com>
2019-05-27 10:30:21 +0300

    project and profile models added to admin.py

--- prize/admin.py
+++ prize/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile,Projects
+
+class ImageAdmin(admin.ModelAdmin):
+    admin.site.register(Profile)
+    admin.site.register(Projects)
 
 # Register your models here.
