63d61660ca8eb4663c2afed933e6fa1d01fff0ba
Tashanah Muhando <tashanah.muhando@gmail.com>
2019-05-27 09:00:15 +0300

    profile class created

--- prize/models.py
+++ prize/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime as dt
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# Create your models here.
+class Profile(models.Model):
+    profile_photo=models.ImageField(upload_to='profiles/',default='download.jpeg')
+    bio=models.TextField(max_length=500) 
+    profile_name=models.CharField(max_length =30,blank=True)
+    user=models.OneToOneField(User,on_delete=models.CASCADE)
