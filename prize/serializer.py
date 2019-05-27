826f3cd4582001dbec69635c2f1904ef441bf885
Tashanah Muhando <tashanah.muhando@gmail.com>
2019-05-27 13:22:29 +0300

    meta serializer created for projects class

--- prize/serializer.py
+++ prize/serializer.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
 from .models import Profile, Projects
 
-class MerchSerializer(serializers.ModelSerializer):
+class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = ('profile_photo', 'bio', 'profile_name', 'user')
+
+class ProjectsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Projects
+        fields = ('profile', 'title', 'image_landing', 'description', 'link')
