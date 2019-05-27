cc0e0b0807d51811ebf8e7a620e44b142e5f41ab
Tashanah Muhando <tashanah.muhando@gmail.com>
2019-05-27 09:56:34 +0300

    database settings changed

--- awards/settings.py
+++ awards/settings.py
@@ -78,8 +78,10 @@ WSGI_APPLICATION = 'awards.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'prize',
+        'USER':'tashanah',
+        'PASSWORD':'tash1234',
     }
 }
 
