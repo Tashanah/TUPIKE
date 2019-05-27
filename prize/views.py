f3caabc1adbead428ef0f2a03b86d70b3f54c859
Tashanah Muhando <tashanah.muhando@gmail.com>
2019-05-27 09:16:30 +0300

    login required function added

--- prize/views.py
+++ prize/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponse,Http404
+from django.contrib.auth.decorators import login_required
+import datetime as dt 
+from .models import *
+from . forms import UserUploadImage
 
-# Create your views here.
+@login_required(login_url='/accounts/login/')
+def home(request):
+
+    projects=Project.objects.all()
+    context = {
+        "projects":projects,
+    }
+
+    return render(request,'project.html',context)
